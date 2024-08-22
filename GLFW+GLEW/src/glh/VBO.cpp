4fb8484410689da6200fe2d0b20d243b017b1b19
Mason Speck <mcs.5299@gmail.com>
2024-08-21 22:06:46 -0400

    added GL_ERROR_CHECK to VBO

--- GLFW+GLEW/src/glh/VBO.cpp
+++ GLFW+GLEW/src/glh/VBO.cpp
@@ -1,4 +1,5 @@
 #include "glh/VBO.h"
+#include "glh/macros.h"
 
 #include <iostream>
 
@@ -30,7 +31,7 @@ static DebugInfoVBO* debugInfoVBO = DebugInfoVBO::getInstance();
 do {                                                                                       \
     if (expectedBoundVBO == 0) continue;                                                   \
     if (expectedBoundVBO != debugInfoVBO->currentlyBoundVBO) {                             \
-        std::cerr << "glh::VBO::" << funcName << "()"                                     \
+        std::cerr << "glh::VBO::" << funcName << "()"                                      \
             << " VBO expected=" <<  expectedBoundVBO                                       \
             << " VBO current=" << debugInfoVBO->currentlyBoundVBO << '\n';                 \
     }                                                                                      \
@@ -48,37 +49,62 @@ do {                                                                            
 namespace glh {
     namespace VBO {
         void create(GLuint& VBO) {
+            GL_ERROR_CHECK("VBO", "create", "Prexisting");
+
             glGenBuffers(1, &VBO);
+
+            GL_ERROR_CHECK("VBO", "create", "glGenBuffers");
         }
 
         void create(GLsizei num, GLuint* VBOArray) {
+            GL_ERROR_CHECK("VBO", "create", "Prexisting");
+
             glGenBuffers(num, VBOArray);
+
+            GL_ERROR_CHECK("VBO", "create", "glGenBuffers");
         }
 
         void destroy(GLuint& VBO) {
+            GL_ERROR_CHECK("VBO", "destroy", "Prexisting");
+
             glDeleteBuffers(1, &VBO);
+
+            GL_ERROR_CHECK("VBO", "destroy", "glDeleteBuffers");
         }
 
         void destroy(GLsizei num, GLuint* VBOArray) {
+            GL_ERROR_CHECK("VBO", "destroy", "Prexisting");
+
             glDeleteBuffers(num, VBOArray);
+
+            GL_ERROR_CHECK("VBO", "destroy", "glDeleteBuffers");
         }
 
         void bind(GLuint VBO) {
+            GL_ERROR_CHECK("VBO", "bind", "Prexisting");
             TRACK_BOUND_VBO(VBO);
 
             glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+            GL_ERROR_CHECK("VBO", "bind", "glBindBuffer");
         }
 
         void allocateBuffer(GLsizeiptr size, const void* data, GLenum usage, GLuint expectedBoundVBO) {
+            GL_ERROR_CHECK("VBO", "allocateBuffer", "Prexisting");
             VERIFY_BOUND_VBO(expectedBoundVBO, "allocateBuffer");
 
             glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+
+            GL_ERROR_CHECK("VBO", "allocateBuffer", "glBufferData");
         }
 
         void updateBuffer(GLintptr offset, GLsizeiptr size, const void* data, GLuint expectedBoundVBO) {
+            GL_ERROR_CHECK("VBO", "updateBuffer", "Prexisting");
             VERIFY_BOUND_VBO(expectedBoundVBO, "updateBuffer");
 
             glBufferSubData(GL_ARRAY_BUFFER, offset, size, data);
+
+            GL_ERROR_CHECK("VBO", "updateBuffer", "glBufferSubData");
         }
     }
 }
