0fbedab81321c48c181a506de7203f7f6b0b0720
Mason Speck <mcs.5299@gmail.com>
2024-08-21 22:00:14 -0400

    added GL_ERROR_CHECK use to UBO

--- GLFW+GLEW/src/glh/UBO.cpp
+++ GLFW+GLEW/src/glh/UBO.cpp
@@ -1,4 +1,5 @@
 #include "glh/UBO.h"
+#include "glh/macros.h"
 
 #include <iostream>
 
@@ -26,14 +27,14 @@ DebugInfoUBO* DebugInfoUBO::instance = nullptr;
 
 static DebugInfoUBO* debugInfoUBO = DebugInfoUBO::getInstance();
 
-#define VERIFY_BOUND_UBO(expectedBoundUBO, funcName)                                       \
-do {                                                                                       \
-    if (expectedBoundUBO == 0) continue;                                                   \
-    if (expectedBoundUBO != debugInfoUBO->currentlyBoundUBO) {                             \
-        std::cerr << "glh::UBO::" << funcName << "()"                                     \
-            << " UBO expected=" <<  expectedBoundUBO                                       \
-            << " UBO current=" << debugInfoUBO->currentlyBoundUBO << '\n';                 \
-    }                                                                                      \
+#define VERIFY_BOUND_UBO(expectedBoundUBO, funcName)                        \
+do {                                                                        \
+    if (expectedBoundUBO == 0) continue;                                    \
+    if (expectedBoundUBO != debugInfoUBO->currentlyBoundUBO) {              \
+        std::cerr << "glh::UBO::" << funcName << "()"                       \
+            << " UBO expected=" <<  expectedBoundUBO                        \
+            << " UBO current=" << debugInfoUBO->currentlyBoundUBO << '\n';  \
+    }                                                                       \
 } while(0)
 
 #define TRACK_BOUND_UBO(newUBO) debugInfoUBO->currentlyBoundUBO = newUBO
@@ -48,41 +49,70 @@ do {                                                                            
 namespace glh {
     namespace UBO {
         void create(GLuint& UBO) {
+            GL_ERROR_CHECK("UBO", "create", "Prexisting");
+
             glGenBuffers(1, &UBO);
+
+            GL_ERROR_CHECK("UBO", "create", "glGenBuffers");
         }
 
         void create(GLsizei num, GLuint* UBOArray) {
+            GL_ERROR_CHECK("UBO", "create", "Prexisting");
+
             glGenBuffers(num, UBOArray);
+
+            GL_ERROR_CHECK("UBO", "create", "glGenBuffers");
         }
 
         void destroy(GLuint& UBO) {
+            GL_ERROR_CHECK("UBO", "destroy", "Prexisting");
+
             glDeleteBuffers(1, &UBO);
+
+            GL_ERROR_CHECK("UBO", "destroy", "glDeleteBuffers");
         }
 
         void destroy(GLsizei num, GLuint* UBOArray) {
+            GL_ERROR_CHECK("UBO", "destroy", "Prexisting");
+
             glDeleteBuffers(num, UBOArray);
+
+            GL_ERROR_CHECK("UBO", "destroy", "glDeleteBuffers");
         }
 
         void bind(GLuint UBO) {
+            GL_ERROR_CHECK("UBO", "bind", "Prexisting");
             TRACK_BOUND_UBO(UBO);
 
             glBindBuffer(GL_UNIFORM_BUFFER, UBO);
+
+            GL_ERROR_CHECK("UBO", "bind", "glBindBuffer");
         }
 
         void allocateBuffer(GLsizeiptr size, const void* data, GLenum usage, GLuint expectedBoundUBO) {
+            GL_ERROR_CHECK("UBO", "allocateBuffer", "Prexisting");
             VERIFY_BOUND_UBO(expectedBoundUBO, "allocateBuffer");
 
             glBufferData(GL_UNIFORM_BUFFER, size, data, usage);
+
+            GL_ERROR_CHECK("UBO", "allocateBuffer", "glBufferData");
         }
 
         void updateBuffer(GLintptr offset, GLsizeiptr size, const void* data, GLuint expectedBoundUBO) {
+            GL_ERROR_CHECK("UBO", "updateBuffer", "Prexisting");
             VERIFY_BOUND_UBO(expectedBoundUBO, "updateBuffer");
 
             glBufferSubData(GL_UNIFORM_BUFFER, offset, size, data);
+
+            GL_ERROR_CHECK("UBO", "updateBuffer", "glBufferSubData");
         }
 
         void bindBufferBase(GLuint bindingPoint, GLuint UBO) {
+            GL_ERROR_CHECK("UBO", "bindBufferBase", "Prexisting");
+
             glBindBufferBase(GL_UNIFORM_BUFFER, bindingPoint, UBO);
+
+            GL_ERROR_CHECK("UBO", "bindBufferBase", "glBindBufferBase");
         }
     }
 }
